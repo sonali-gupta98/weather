4c7b1d00f46dcf1ce1d0a0e0381e3d8bd74f91fb
sonali-gupta98 <82636174+sonali-gupta98@users.noreply.github.com>
2021-05-04 18:47:22 -0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,52 @@
+const api = {
+    key: "52a9222e696c325bf0096e5da7d0add1",
+    base: "http://api.openweathermap.org/data/2.5/"
+}
+
+const searchbox= document.querySelector('.search-box');
+searchbox.addEventListener('keypress', setQuery);
+
+function setQuery(evt) {
+    if (evt.keyCode == 13) {
+        getResults(searchbox.value);
+    }
+}
+
+function getResults (query) {
+    fetch(`${api.base}weather?q=${query}&units=matric&APPID=${api.key}`)
+     .then(weather => {
+         return weather.json();
+     }).then(displayResults);
+} 
+
+function displayResults (weather) {
+    console.log(weather);
+    let city = document.querySelector('.location .city');
+    city.innerText = `${weather.name}, ${weather.sys.country}`;
+
+    let now = new Date();
+    let date = document.querySelector('.location .date');
+    date.innerText = dateBuilder(now);
+
+    let temp = document.querySelector('.current .temp');
+    temp.innerHTML = `${Math.round(weather.main.temp)}<span>°F</span>`;
+
+    let locationIcon = document.querySelector('.current .temperature .weather-icon');
+    locationIcon.innerHTML = `<img src="icons/${weather.weather[0].icon}.png">`;
+
+    let weather_el = document.querySelector('.current .weather');
+    weather_el.innerText = weather.weather[0].main;
+
+    let hilow = document.querySelector('.hi-low');
+    hilow.innerText = `${Math.round(weather.main.temp_min)}°F / ${Math.round(weather.main.temp_max)}°F`;
+}
+function dateBuilder (d) {
+    let months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+    let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+    let day = days[d.getDay()];
+    let date = d.getDate();
+    let month = months[d.getMonth()];
+    let year = d.getFullYear();
+
+    return `${day} ${date} ${month} ${year}`;
+}
